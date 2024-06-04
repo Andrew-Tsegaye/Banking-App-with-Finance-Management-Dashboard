d91ab160b03c4f72f1b0c7de5684a179147657e2
Andrew Tsegaye <andrewtsegaye7@gmail.com>
2024-06-04 23:04:05 +0300

    desktop sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,16 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Andrew", lastName: "JSM" };
+
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
       {children}
     </main>
   );
