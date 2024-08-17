cf38cf5204673b7a086e5e1d3076997bd7e47ca8
Andrew Tsegaye <andrewtsegaye7@gmail.com>
2024-08-17 23:14:05 +0300

    fix small bug

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -68,7 +68,7 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
             width={24}
             height={24}
           />
-          <p className="hiddenl text-[16px] font-semibold text-black-2 xl:block">
+          <p className="hidden text-[16px] font-semibold text-black-2 xl:block">
             Connect bank
           </p>
         </Button>
@@ -80,7 +80,9 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
             width={24}
             height={24}
           />
-          <p className="text-[16px] font-semibold text-black-2">Connect bank</p>
+          <p className="sidebar-label text-[16px] font-semibold text-black-2">
+            Connect bank
+          </p>
         </Button>
       )}
     </>
